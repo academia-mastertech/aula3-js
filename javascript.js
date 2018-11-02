4884633bf1a5293d04b5c52c3262151d8fb53dcf
wesleyclzns <calazanscafe@outlook.com>
2018-11-01 21:55:20 -0300

    criação de arquivos

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let nota = Number(prompt("Qual sua Nota:"));
+let entregou = confirm ("entregou os trabalhos?")
+
+if (nota >=6) {
+  alert ("Parabens vc passou!");
+}
+
+  else if (nota >= 5.5) {
+    alert("Eu te passei, me agradeça humano não inteligente")
+  }
+
+  else {
+    alert("Tente Novamente, pq @pessoa vc reprovou");
+  }
---
+++ script.js
@@ -0,0 +1,9 @@
+let nota = Number(prompt("QUal a sua nota?"))
+
+  if (nota < 5.5) {
+    alert("Nos vemos ano que vem, foi reprovado")
+  }
+
+  else {
+
+  }
